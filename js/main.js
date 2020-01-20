389283f122e32c76c6fd45bfe08f7e6fcec7f591
wolfchara <vovikys1997@gmail.com>
2020-01-20 16:51:39 +0200

    fixed services

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.maxHeight) {
@@ -13,27 +13,35 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
-$(document).ready(function(){
-$(".gallery__wedding .wedding__title, .owl-carousel").owlCarousel({
-  loop: true,
-  margin: 10,
-  nav: true,
-  responsive: {
-    0: {
-      items: 1
-    },
-    440: {
-      items: 2
-    },
-    600: {
-      items: 3
-    },
-    800: {
-      items: 4
-    },
-    1000: {
-      items: 5
+$(document).ready(function () {
+  $(".gallery__wedding .wedding__title, .owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      440: {
+        items: 2
+      },
+      600: {
+        items: 3
+      },
+      800: {
+        items: 4
+      },
+      1000: {
+        items: 5
+      }
     }
-  }
+  });
 });
-});
+
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+};
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+};
